1611475378ebffccda32d34cc8c3e7b9aee127d5
Lee Scholl <v1xiii@gmail.com>
2020-01-13 21:28:38 -0600

    still have no idea what the problem is, surprise surprise

--- app/src/main/java/wgu/lschol1/c196/Terms.java
+++ app/src/main/java/wgu/lschol1/c196/Terms.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -28,8 +29,10 @@ public class Terms extends AppCompatActivity {
         final TermsAdapter adapter = new TermsAdapter(this);
         termsRecyclerView.setAdapter(adapter);
         termsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mTermsViewModel = new ViewModelProvider(this).get(TermsViewModel.class); // cannot resolve constructor ViewModelProvider(wgu.lschol1.c196.Terms)
 
-        //mTermsViewModel = new ViewModelProvider(this).get(TermsViewModel.class); // cannot resolve constructor ViewModelProvider(wgu.lschol1.c196.Terms)
+        // when opening terms page
+        //W/ActivityThread: handleWindowVisibility: no activity for token android.os.BinderProxy@3f9c11c
     }
 
     public void openTermDetailsPage(View view) {
